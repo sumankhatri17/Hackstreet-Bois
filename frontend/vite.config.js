9f3464a3612ca405e313a0dca145915aa214dc90
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2025-12-24 11:00:38 +0545

    Remove @tailwindcss/vite plugin and use standard React plugin configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -7,8 +6,7 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler'],
-        tailwindcss()],
+        plugins: ['babel-plugin-react-compiler'],
       },
     }),
   ],
